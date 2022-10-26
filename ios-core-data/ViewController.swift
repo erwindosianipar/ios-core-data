3aa2fb7b02b3fcd9c4e87a67986387f33194580f
Erwindo Sianipar <erwindosianipar@gmail.com>
2022-10-26 14:15:25 +0700

    Initial Commit

---
+++ ios-core-data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-core-data
+//
+//  Created by Erwindo Sianipar on 10/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
